4a9acd77aee6fe3647d4605b57e6e90db0c5d1ce
Mehdi <selmimehdi970@gmail.com>
2026-04-17 11:07:31 +0100

    add file navbar & install react-icons

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { IoIosSearch } from "react-icons/io";
+import { FaRegUser } from "react-icons/fa";
+import { MdOutlineShoppingBag } from "react-icons/md";
+const Navbar = () => {
+  return (
+    <div>
+
+        <IoIosSearch />
+        <FaRegUser />
+<MdOutlineShoppingBag />
+      <h3>sdfdsf</h3>
+    </div>
+  );
+};
+
+export default Navbar;
